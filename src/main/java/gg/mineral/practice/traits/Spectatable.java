40680989b0e0725fc461595dfc54ced6454e1e0b
Jaiden <jaibrown51@gmail.com>
2022-12-21 02:22:09 +0000

    Specatatable type

--- src/main/java/gg/mineral/practice/traits/Spectatable.java
+++ src/main/java/gg/mineral/practice/traits/Spectatable.java
@@ -6,6 +6,11 @@ import gg.mineral.practice.entity.Spectator;
 import gg.mineral.practice.util.collection.ProfileList;
 
 public interface Spectatable {
+
+    public enum Type {
+        EVENT, MATCH
+    }
+
     ConcurrentLinkedDeque<Spectator> spectators = new ConcurrentLinkedDeque<>();
 
     default ConcurrentLinkedDeque<Spectator> getSpectators() {
