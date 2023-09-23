cba292fbc6aeb915ca5d540f383088cd4bc1198a
Jaiden <48765522+jaidennnnn@users.noreply.github.com>
2023-09-24 00:44:30 +0100

    Update custom kb menu

--- src/main/java/gg/mineral/practice/inventory/menus/CreateCustomKnockbackMenu.java
+++ src/main/java/gg/mineral/practice/inventory/menus/CreateCustomKnockbackMenu.java
@@ -31,52 +31,70 @@ public class CreateCustomKnockbackMenu extends PracticeMenu {
                 : viewer.getMatchData().getCustomKnockback();
         viewer.getMatchData().setCustomKnockback(kb);
 
-        String frictionStr = "Friction: " + DECIMAL_FORMAT.format(kb.getFriction());
-        String horizontalStr = "Horizontal: " + DECIMAL_FORMAT.format(kb.getHorizontal());
-        String extraHorizontalStr = "Sprinting/W Tap Horizontal: " + DECIMAL_FORMAT.format(kb.getExtraHorizontal());
-        String verticalStr = "Vertical: " + DECIMAL_FORMAT.format(kb.getVertical());
-        String extraVerticalStr = "Sprinting/W Tap Vertical: " + DECIMAL_FORMAT.format(kb.getExtraVertical());
-        String verticalLimitStr = "Vertical Limit: " + DECIMAL_FORMAT.format(kb.getVerticalLimit());
+        setSlot(0, ItemStacks.FRICTION.name(CC.SECONDARY + CC.B + "Friction")
+                .lore(CC.WHITE + "The amount " + CC.SECONDARY + "movement speed and direction",
+                        CC.WHITE + "influences knockback magnitude.", " ",
+                        CC.WHITE + "Currently:", CC.GOLD + DECIMAL_FORMAT.format(kb.getFriction()),
+                        CC.BOARD_SEPARATOR, CC.ACCENT + "Click to change value.")
+                .build(), interaction -> {
+                    interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
+                        kb.knockbackFriction = value;
+                    }));
+                });
 
-        setSlot(0, ItemStacks.FRICTION.name(frictionStr).build(), interaction -> {
-            interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
-                kb.knockbackFriction = value;
-            }));
-        });
+        setSlot(1, ItemStacks.HORIZONTAL.name(CC.SECONDARY + CC.B + "Horizontal")
+                .lore(CC.WHITE + "The " + CC.SECONDARY + "horizontal" + CC.WHITE + " knockback.", " ",
+                        CC.WHITE + "Currently:", CC.GOLD + DECIMAL_FORMAT.format(kb.getHorizontal()),
+                        CC.BOARD_SEPARATOR, CC.ACCENT + "Click to change value.")
+                .build(), interaction -> {
+                    interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
+                        kb.knockbackHorizontal = value;
+                    }));
+                });
 
-        setSlot(1, ItemStacks.HORIZONTAL.name(horizontalStr).build(), interaction -> {
-            interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
-                kb.knockbackHorizontal = value;
-            }));
-        });
+        setSlot(2, ItemStacks.VERTICAL.name(CC.SECONDARY + CC.B + "Vertical")
+                .lore(CC.WHITE + "The " + CC.SECONDARY + "vertical" + CC.WHITE + " knockback.", " ",
+                        CC.WHITE + "Currently:", CC.GOLD + DECIMAL_FORMAT.format(kb.getVertical()),
+                        CC.BOARD_SEPARATOR, CC.ACCENT + "Click to change value.")
+                .build(), interaction -> {
+                    interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
+                        kb.knockbackVertical = value;
+                    }));
+                });
 
-        setSlot(2, ItemStacks.VERTICAL.name(verticalStr).build(), interaction -> {
-            interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
-                kb.knockbackVertical = value;
-            }));
-        });
-
-        setSlot(4, ItemStacks.CLICK_TO_APPLY_CHANGES.name("Save Knockback").build(), interaction -> {
+        setSlot(4, ItemStacks.APPLY, interaction -> {
             Profile p = interaction.getProfile();
             p.getMatchData().setKnockback(kb);
             p.openMenu(menu);
         });
 
         setSlot(6, ItemStacks.EXTRA_HORIZONTAL
-                .name(extraHorizontalStr).build(), interaction -> {
+                .name(CC.SECONDARY + CC.B + "Extra Horizontal")
+                .lore(CC.WHITE + "The " + CC.SECONDARY + "horizontal knockback" + CC.WHITE + " added",
+                        CC.WHITE + "when sprinting/sprint resetting.", " ",
+                        CC.WHITE + "Currently:", CC.GOLD + DECIMAL_FORMAT.format(kb.getExtraHorizontal()),
+                        CC.BOARD_SEPARATOR, CC.ACCENT + "Click to change value.")
+                .build(), interaction -> {
                     interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
                         kb.knockbackExtraHorizontal = value;
                     }));
                 });
 
-        setSlot(7, ItemStacks.EXTRA_VERTICAL.name(extraVerticalStr)
+        setSlot(7, ItemStacks.EXTRA_VERTICAL.name(CC.SECONDARY + CC.B + "Extra Vertical")
+                .lore(CC.WHITE + "The " + CC.SECONDARY + "vertical knockback" + CC.WHITE + " added",
+                        CC.WHITE + "when sprinting/sprint resetting.", " ",
+                        CC.WHITE + "Currently:", CC.GOLD + DECIMAL_FORMAT.format(kb.getExtraVertical()),
+                        CC.BOARD_SEPARATOR, CC.ACCENT + "Click to change value.")
                 .build(), interaction -> {
                     interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
                         kb.knockbackExtraVertical = value;
                     }));
                 });
 
-        setSlot(8, ItemStacks.VERTICAL_LIMIT.name(verticalLimitStr)
+        setSlot(8, ItemStacks.VERTICAL_LIMIT.name(CC.SECONDARY + CC.B + "Vertical Limit")
+                .lore(CC.WHITE + "The " + CC.SECONDARY + "limit" + CC.WHITE + " to vertical knockback.", " ",
+                        CC.WHITE + "Currently:", CC.GOLD + DECIMAL_FORMAT.format(kb.getVerticalLimit()),
+                        CC.BOARD_SEPARATOR, CC.ACCENT + "Click to change value.")
                 .build(), interaction -> {
                     interaction.getProfile().openMenu(new ConfigureKnockbackValueMenu(this, value -> {
                         kb.knockbackVerticalLimit = value;
