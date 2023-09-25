4b62de0d1c2a4f2f81761e203771d9b2c1b6fa0a
Jaiden <48765522+jaidennnnn@users.noreply.github.com>
2023-09-25 01:34:42 +0100

    Scoreboard viewable for spectators

--- src/main/java/gg/mineral/practice/scoreboard/impl/SpectatorScoreboard.java
+++ src/main/java/gg/mineral/practice/scoreboard/impl/SpectatorScoreboard.java
@@ -1,8 +1,13 @@
 package gg.mineral.practice.scoreboard.impl;
 
 import gg.mineral.practice.entity.Profile;
+import gg.mineral.practice.match.Match;
+import gg.mineral.practice.match.PartyMatch;
+import gg.mineral.practice.match.TeamMatch;
 import gg.mineral.practice.scoreboard.Scoreboard;
 import gg.mineral.practice.scoreboard.ScoreboardHandler;
+import gg.mineral.practice.traits.Spectatable;
+import gg.mineral.practice.util.collection.ProfileList;
 import gg.mineral.practice.util.messages.CC;
 
 public class SpectatorScoreboard
@@ -13,9 +18,121 @@ public class SpectatorScoreboard
     @Override
     public void updateBoard(ScoreboardHandler board, Profile profile) {
         board.updateTitle(CC.PRIMARY + CC.B + "Mineral");
-        board.updateLines(CC.BOARD_SEPARATOR,
-                CC.ACCENT + "Spectating", CC.SPACER,
-                CC.SECONDARY + "mineral.gg",
-                CC.BOARD_SEPARATOR);
+
+        Spectatable spectatable = profile.getSpectateHandler().getSpectatable();
+
+        if (spectatable instanceof TeamMatch) {
+            TeamMatch match = (TeamMatch) spectatable;
+
+            ProfileList team = match.getTeam(match.getProfile1());
+            ProfileList opponents = new ProfileList(match.getParticipants());
+            opponents.removeAll(team);
+
+            if (match.getData().getBoxing()) {
+
+                int hitCount = match.getTeam1HitCount();
+                int opponentHitCount = match.getTeam2HitCount();
+                int requiredHitCount = match.getTeam1RequiredHitCount();
+                int opponentRequiredHitCount = match.getTeam2RequiredHitCount();
+
+                board.updateLines(CC.BOARD_SEPARATOR,
+                        CC.SECONDARY + match.getProfile1().getName(),
+                        CC.YELLOW + " * " + CC.ACCENT + "Hits: "
+                                + CC.WHITE + hitCount + "/" + requiredHitCount,
+                        CC.SECONDARY
+                                + match.getProfile2().getName(),
+                        CC.YELLOW + " * " + CC.ACCENT + "Hits: " + CC.WHITE
+                                + opponentHitCount + "/" + opponentRequiredHitCount,
+                        CC.SPACER,
+                        CC.SECONDARY + "mineral.gg",
+                        CC.BOARD_SEPARATOR);
+                return;
+            }
+
+            board.updateLines(CC.BOARD_SEPARATOR,
+                    CC.SECONDARY + match.getProfile1().getName(),
+                    CC.YELLOW + " * " + CC.ACCENT + "Team Remaining: "
+                            + CC.WHITE + team.size(),
+                    CC.SECONDARY
+                            + match.getProfile2().getName(),
+                    CC.YELLOW + " * " + CC.ACCENT + "Team Remaining: " + CC.WHITE
+                            + opponents.size(),
+                    CC.SPACER,
+                    CC.SECONDARY + "mineral.gg",
+                    CC.BOARD_SEPARATOR);
+        } else if (spectatable instanceof PartyMatch) {
+            PartyMatch match = (PartyMatch) spectatable;
+
+            ProfileList team = match.getTeam(match.getProfile1());
+            ProfileList opponents = new ProfileList(match.getParticipants());
+            opponents.removeAll(team);
+
+            if (match.getData().getBoxing()) {
+
+                int hitCount = match.getTeam1HitCount();
+                int opponentHitCount = match.getTeam2HitCount();
+                int requiredHitCount = match.getTeam1RequiredHitCount();
+                int opponentRequiredHitCount = match.getTeam2RequiredHitCount();
+
+                board.updateLines(CC.BOARD_SEPARATOR,
+                        CC.SECONDARY + match.getProfile1().getName(),
+                        CC.YELLOW + " * " + CC.ACCENT + "Hits: "
+                                + CC.WHITE + hitCount + "/" + requiredHitCount,
+                        CC.SECONDARY
+                                + match.getProfile2().getName(),
+                        CC.YELLOW + " * " + CC.ACCENT + "Hits: " + CC.WHITE
+                                + opponentHitCount + "/" + opponentRequiredHitCount,
+                        CC.SPACER,
+                        CC.SECONDARY + "mineral.gg",
+                        CC.BOARD_SEPARATOR);
+                return;
+            }
+
+            board.updateLines(CC.BOARD_SEPARATOR,
+                    CC.SECONDARY + match.getProfile1().getName(),
+                    CC.YELLOW + " * " + CC.ACCENT + "Team Remaining: "
+                            + CC.WHITE + team.size(),
+                    CC.SECONDARY
+                            + match.getProfile2().getName(),
+                    CC.YELLOW + " * " + CC.ACCENT + "Team Remaining: " + CC.WHITE
+                            + opponents.size(),
+                    CC.SPACER,
+                    CC.SECONDARY + "mineral.gg",
+                    CC.BOARD_SEPARATOR);
+        } else if (spectatable instanceof Match) {
+            Match match = (Match) spectatable;
+
+            if (match.getData().getBoxing()) {
+
+                board.updateLines(CC.BOARD_SEPARATOR,
+                        CC.SECONDARY + match.getProfile1().getName(),
+                        CC.YELLOW + " * " + CC.ACCENT + "Ping: "
+                                + CC.WHITE + match.getProfile1().getPlayer().getHandle().ping,
+                        CC.YELLOW + " * " + CC.ACCENT + "Hits: "
+                                + CC.WHITE + match.getProfile1().getMatchStatisticCollector().getHitCount(),
+                        CC.SECONDARY
+                                + match.getProfile2().getName(),
+                        CC.YELLOW + " * " + CC.ACCENT + "Ping: " + CC.WHITE
+                                + match.getProfile2().getPlayer().getHandle().ping,
+                        CC.YELLOW + " * " + CC.ACCENT + "Hits: "
+                                + CC.WHITE + match.getProfile2().getMatchStatisticCollector().getHitCount(),
+                        CC.SPACER,
+                        CC.SECONDARY + "mineral.gg",
+                        CC.BOARD_SEPARATOR);
+                return;
+            }
+
+            board.updateLines(CC.BOARD_SEPARATOR,
+                    CC.SECONDARY + match.getProfile1().getName(),
+                    CC.YELLOW + " * " + CC.ACCENT + "Ping: "
+                            + CC.WHITE + match.getProfile1().getPlayer().getHandle().ping,
+                    CC.SECONDARY
+                            + match.getProfile2().getName(),
+                    CC.YELLOW + " * " + CC.ACCENT + "Ping: " + CC.WHITE
+                            + match.getProfile2().getPlayer().getHandle().ping,
+                    CC.SPACER,
+                    CC.SECONDARY + "mineral.gg",
+                    CC.BOARD_SEPARATOR);
+        }
     }
 }
