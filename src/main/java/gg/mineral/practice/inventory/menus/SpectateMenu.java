500d1ff20b9e0d16d0bd8ac7953cebb7fef6f06f
jaidennnnn <48765522+jaidennnnn@users.noreply.github.com>
2024-11-14 22:03:34 +0000

    Make spectate menu use uuid

--- src/main/java/gg/mineral/practice/inventory/menus/SpectateMenu.java
+++ src/main/java/gg/mineral/practice/inventory/menus/SpectateMenu.java
@@ -1,11 +1,13 @@
 package gg.mineral.practice.inventory.menus;
 
+import gg.mineral.practice.entity.PlayerStatus;
 import gg.mineral.practice.inventory.ClickCancelled;
 import gg.mineral.practice.inventory.PracticeMenu;
 import gg.mineral.practice.managers.MatchManager;
-
+import gg.mineral.practice.managers.ProfileManager;
 import gg.mineral.practice.util.items.ItemBuilder;
 import gg.mineral.practice.util.messages.CC;
+import gg.mineral.practice.util.messages.impl.ErrorMessages;
 import lombok.val;
 
 @ClickCancelled(true)
@@ -27,8 +29,20 @@ public class SpectateMenu extends PracticeMenu {
                             CC.GOLD + m.getData().getGametype().getName(),
                             CC.BOARD_SEPARATOR, CC.ACCENT + "Click to spectate.")
                     .build();
-            add(skull, interaction -> interaction.getProfile().getPlayer()
-                    .performCommand("spec " + m.getParticipants().getFirst().getName()));
+            add(skull, interaction -> {
+                val profile = interaction.getProfile();
+
+                val profileToSpectate = ProfileManager
+                        .getProfile(m.getParticipants().getFirst().getUuid(),
+                                p -> p.getPlayerStatus() == PlayerStatus.FIGHTING || p.isInEvent());
+
+                if (profileToSpectate == null) {
+                    profile.message(ErrorMessages.PLAYER_NOT_IN_MATCH_OR_EVENT);
+                    return;
+                }
+
+                profile.getSpectateHandler().spectate(profileToSpectate);
+            });
         }
     }
 
